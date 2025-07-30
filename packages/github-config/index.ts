e9c8dceaaf6ba6d0858d934492fdabef2ba84414
Bibek Tamang <bibek7here@gmail.com>
2025-07-30 23:08:24 +0545

    added github app installation

--- packages/github-config/index.ts
+++ packages/github-config/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { Webhooks } from "@octokit/webhooks";
+import { App, Octokit } from "octokit";
+
+const GITHUB_APP_ID = process.env.GITHUB_APP_ID;
+const GITHUB_APP_PRIVATE_KEY = process.env.GITHUB_PRIVATE_KEY;
+const GITHUB_APP_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
+
+const app = new App({
+	appId: GITHUB_APP_ID!,
+	privateKey: GITHUB_APP_PRIVATE_KEY!,
+});
+const webhooks = new Webhooks({ secret: GITHUB_APP_WEBHOOK_SECRET! });
+
+async function getInstallationOctokit(installationId: number) {
+	const token = await app.getInstallationOctokit(installationId);
+	return new Octokit({ auth: token });
+}
+
+export { app as GithubApp, webhooks, getInstallationOctokit };
