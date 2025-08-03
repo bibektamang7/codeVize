66745b19e9547e9c9711c92785e689b51393909a
Bibek Tamang <bibek7here@gmail.com>
2025-08-03 23:02:05 +0545

    middleware init

---
+++ apps/web/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./app/api/auth/[...nextauth]/auth";
+
+async function middleware(req: NextRequest) {
+	const session = await auth();
+	const path = req.nextUrl.pathname;
+
+	const isPublicPath = path === "/login" || path === "/signup";
+
+	if (isPublicPath && session) {
+		return NextResponse.redirect(new URL("/dashboard", req.url));
+	}
+	if (!isPublicPath && !session) {
+		return NextResponse.redirect(new URL("/login", req.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/login", "/signup", "/dashboard"],
+};
+
+export default middleware;
