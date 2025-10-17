ea9f939b54274039e8540c4034b4380b3ef13afa
Bibek Tamang <bibek7here@gmail.com>
2025-10-17 21:56:06 +0545

    added worker

---
+++ apps/worker/index.ts
@@ -0,0 +1,39 @@
+import { Redis } from "ioredis";
+import { Worker } from "bullmq";
+import { logger } from "logger";
+import type { Job } from "bullmq";
+import { prCodeReview, deleteEmbedRepo, embedRepoChain } from "langflows";
+
+export const redisConnection = new Redis(process.env.REDIS_URL!, {
+	maxRetriesPerRequest: null,
+	enableReadyCheck: false,
+});
+
+const repoWorker = new Worker(
+	"github-job",
+	async (job: Job) => {
+		switch (job.name) {
+			case "review": {
+				const { owner, prNumber, installationId, repoId, repoName } = job.data;
+				await prCodeReview(prNumber, installationId, owner, repoId, repoName);
+				break;
+			}
+
+			case "embed-repo": {
+				const { installationId, owner, repo } = job.data;
+				await embedRepoChain(installationId, owner, repo);
+				break;
+			}
+
+			case "delete-embedding":
+				await deleteEmbedRepo(job.data);
+				break;
+		}
+	},
+	{
+		connection: redisConnection as any,
+	}
+);
+repoWorker.on("failed", (job, err) => {
+	logger.error(`Repo job failed`, { jobId: job?.id, error: err.message });
+});
